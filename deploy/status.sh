68da86225c19b1c866cdec1f58f2104d146a4682
Santosh0303 <santoshsujatha92@users.noreply.github.com>
2025-04-17 18:04:52 +0530

    v3

--- deploy/status.sh
+++ deploy/status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SERVER_DETAILS=/u01/app/oracle/middleware/devops/git/git-test/scripts/servers_list
+SERVER_DETAILS=/u01/app/oracle/middleware/devops/git/git-test/deploy/servers_list
 MAIL_FILE=/tmp/mail.txt
 TABLE_FILE=/tmp/status1
 
