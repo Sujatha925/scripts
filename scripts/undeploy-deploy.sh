5f743a47057e2ba06ffe85052596946189d221b0
Jenkins <113046097+Sujatha925@users.noreply.github.com>
2025-04-17 15:29:49 +0530

    Update undeploy-deploy.sh

--- scripts/undeploy-deploy.sh
+++ scripts/undeploy-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IFS=','
-SCRIPT_PATH=/u01/app/oracle/middleware/devops/scripts/scripts
+SCRIPT_PATH=/u01/app/oracle/middleware/devops/git/git-test/scripts
 for i in $Application
 do
 /u01/app/oracle/middleware1/wls12c-silent/oracle_common/common/bin/wlst.sh $SCRIPT_PATH/undeploy-deploy.py $i
